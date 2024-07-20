bed0bbe79ff5d2cf28d4e3d1c5b85c31c29fd542
Vladimir <vladimirrogalev1@gmail.com>
2024-07-20 13:44:12 +0300

    Delete book by  Id

--- frontend/src/redux/books/actionCreators.js
+++ frontend/src/redux/books/actionCreators.js
@@ -1,6 +1,13 @@
-import { ADD_BOOK } from './actionTypes';
+import { ADD_BOOK, DELETE_BOOK } from './actionTypes';
 
 export const addBook = (newBook) => ({
   type: ADD_BOOK,
   payload: newBook,
 });
+
+export const deleteBook = (id) => {
+  return {
+    type: DELETE_BOOK,
+    payload: id,
+  };
+};
--- frontend/src/redux/books/reducer.js
+++ frontend/src/redux/books/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_BOOK } from './actionTypes';
+import { ADD_BOOK, DELETE_BOOK } from './actionTypes';
 
 const initialState = [];
 
@@ -6,6 +6,8 @@ const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
+    case DELETE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
     default:
       return state;
   }
