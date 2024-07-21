9d5cdd7eff044f0c1e3c8d8c9c20fe1def39bd58
Vladimir <vladimirrogalev1@gmail.com>
2024-07-21 21:08:41 +0300

    Created booksSlice instead of regular reducer

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,6 +1,10 @@
 import { useDispatch } from 'react-redux';
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs';
-import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
+import {
+  deleteBook,
+  selectBooks,
+  toggleFavorite,
+} from '../../redux/slices/bookSlice';
 import './BookList.css';
 import { useSelector } from 'react-redux';
 import {
@@ -10,7 +14,7 @@ import {
 } from '../../redux/slices/filterSlice';
 
 const BookList = () => {
-  const books = useSelector((state) => state.books);
+  const books = useSelector(selectBooks);
   const titleFilter = useSelector(selectTitleFilter);
   const authorFilter = useSelector(selectAuthorFilter);
   const onlyFavoriteFilter = useSelector(selectOnlyFavoriteFilter);
---
+++ frontend/src/redux/slices/bookSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+    deleteBook: (state, action) => {
+      //   const index = state.findIndex((book) => book.id === action.payload);
+      //   if (index !== -1) {
+      //     state.splice(index, 1);
+      //   }
+      return state.filter((book) => book.id !== action.payload);
+    },
+    toggleFavorite: (state, action) => {
+      //   return state.map((book) =>
+      state.forEach((book) => {
+        if (book.id === action.payload) {
+          book.isFavorite = !book.isFavorite;
+        }
+      });
+      //     book.id === action.payload
+      //       ? { ...book, isFavorite: !book.isFavorite }
+      //       : book
+      //   );
+    },
+  },
+});
+
+export const { addBook, deleteBook, toggleFavorite } = bookSlice.actions;
+export const selectBooks = (state) => state.books;
+export default bookSlice.reducer;
